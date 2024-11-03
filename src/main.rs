af98130f60a1df6a12ceabcb4061758acdfbdd47
Shantanu Mishra <dev@8hantanu.net>
2024-11-03 16:33:41 +0530

    Fix toggling between edit and preview mode

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@ use axum::{
     Router,
     extract::Form,
 };
-use maud::{html, Markup};
+use maud::{html, Markup, PreEscaped};
 use std::net::SocketAddr;
 use serde::Deserialize;
 use pulldown_cmark::{Parser, Options, html::push_html};
+use html_escape::encode_text;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(render))
-        .route("/preview", post(preview_markdown));
+        .route("/preview", post(preview_markdown))
+        .route("/edit", post(edit_mode));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}", addr);
@@ -34,6 +36,7 @@ fn render_ui() -> Markup {
         head {
             title { "Mdow 🌾" }
             link rel="stylesheet" href="https://yree.io/mold/assets/css/main.css";
+            script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" async="" {}
             script src="https://unpkg.com/htmx.org@1.9.10" {}
         }
         body {
@@ -42,16 +45,17 @@ fn render_ui() -> Markup {
                 p { 
                     b {"A meadow for your markdown files."}
                 }
-                p { "Enter your markdown, preview it, and share it with others. Shared links are valid for a month." }
-                form {
+                p { "Enter your markdown, preview it, and share it with others." }
+                p { "Shared links are valid for a month." }
+                div {
                     div class="grid" {
-                        button type="button" hx-post="/preview" hx-trigger="click" hx-target="#preview-area" hx-swap="innerHTML" hx-include="#markdown-input" { "Preview" }
-                        button { "Share" }
+                        button id="preview-button" type="button" hx-post="/preview" hx-trigger="click" hx-target="#content-area" hx-swap="innerHTML" hx-include="#markdown-input" { "Preview" }
+                        button id="edit-button" type="button" hx-post="/edit" hx-trigger="click" hx-target="#content-area" hx-swap="innerHTML" hx-include="#markdown-preview" style="display: none;" { "Edit" }
+                        button disabled { "Share" }
                     }
-                    div {
-                        textarea id="markdown-input" name="content" placeholder="Enter your markdown..." style="width: 100%" {}
+                    div id="content-area" {
+                        textarea id="markdown-input" name="content" placeholder="Enter your markdown..." style="width: 100%; height: 100%;" {}
                     }
-                    div id="preview-area" class="markdown-preview" {}
                 }
             }
         }
@@ -68,7 +72,33 @@ async fn preview_markdown(Form(input): Form<MarkdownInput>) -> impl IntoResponse
     let mut html_output = String::new();
     push_html(&mut html_output, parser);
     
-    Html(html_output)
+    let preview_markup = html! {
+        div id="markdown-preview" {
+            input type="hidden" name="content" value=(encode_text(&input.content));
+            (PreEscaped(html_output))
+        }
+        script {
+            "document.getElementById('preview-button').style.display = 'none';"
+            "document.getElementById('edit-button').style.display = 'block';"
+        }
+
+        script { "MathJax.typeset();" }
+    };
+
+    Html(preview_markup.into_string())
+}
+
+async fn edit_mode(Form(input): Form<MarkdownInput>) -> impl IntoResponse {
+    let edit_markup = html! {
+        textarea id="markdown-input" name="content" placeholder="Enter your markdown..." style="width: 100%" {
+            (input.content)
+        }
+        script {
+            "document.getElementById('preview-button').style.display = 'block';"
+            "document.getElementById('edit-button').style.display = 'none';"
+        }
+    };
+    Html(edit_markup.into_string())
 }
 
 async fn render() -> impl IntoResponse {

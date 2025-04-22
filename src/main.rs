8a89f03651fa56778fb3595b31cbff26c6af31f1
Shantanu Mishra <dev@8hantanu.net>
2025-04-22 22:01:13 +0530

    Cleanup dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,19 @@
 mod database;
 mod handlers;
 mod utils;
-mod views;
 mod models;
 
 use crate::database::setup_database;
-use crate::views::{handle_main_request, handle_preview_request, handle_edit_request, handle_share_request, handle_view_request, handle_debug_request};
+use crate::handlers::{handle_main_request, handle_preview_request, handle_edit_request, handle_share_request, handle_view_request};
 use crate::models::{MarkdownInput, MarkdownDocument, RenderParams};
 use crate::utils::{create_markdown_editor_page, create_markdown_viewer_page, handle_404, save_markdown_document, generate_short_uuid, create_htmx_redirect_response, clean, convert_markdown_to_html};
 use axum::{
-    extract::{Form, Path, Query, State},
     http::StatusCode,
-    response::{Html, IntoResponse},
     routing::{get, post},
     Router,
 };
-use chrono::{DateTime, Utc};
-use maud::{html, Markup, PreEscaped};
-use pulldown_cmark::{html::push_html, Options, Parser};
-use qrcode::{render::svg, QrCode};
-use serde::Deserialize;
-use sqlx::sqlite::{SqliteConnectOptions, SqliteJournalMode, SqlitePool, SqlitePoolOptions};
+use sqlx::sqlite::SqlitePool;
 use std::net::SocketAddr;
-use std::str::FromStr;
-use std::time::Duration;
-use uuid::Uuid;
 
 const DEFAULT_PORT: u16 = 8081;
 // const DEFAULT_DB_PATH: &str = "sqlite:data/database.db";
@@ -32,24 +21,6 @@ const DEFAULT_DB_PATH: &str = "test.db";
 const DOCUMENT_EXPIRY_DAYS: i64 = 30;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-#[derive(Deserialize)]
-struct MarkdownInput {
-    content: String,
-}
-
-#[derive(sqlx::FromRow)]
-struct MarkdownDocument {
-    id: String,
-    content: String,
-    created_at: DateTime<Utc>,
-    expires_at: DateTime<Utc>,
-}
-
-#[derive(Deserialize)]
-struct RenderParams {
-    content: Option<String>,
-}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -75,7 +46,6 @@ fn setup_router(pool: SqlitePool) -> Router {
         .fallback(|| async { (StatusCode::NOT_FOUND, handle_404()) })
         .with_state(pool)
 }
-
 
 fn get_server_addr() -> SocketAddr {
     let port = std::env::var("PORT")
